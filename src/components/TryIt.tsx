8417d4dd06ad013b5c71316e999e4bb5bab39a99
Xlayton <weightequalsmasstimesgravity@gmail.com>
2020-10-20 16:52:43 -0600

    Canvas on Try It page

--- src/components/TryIt.tsx
+++ src/components/TryIt.tsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
-
+const THREE = require("three.js")
 interface IProps { }
 
 interface IState { }
 
 export class TryIt extends Component<IProps, IState> {
+
+    componentDidMount() {
+        var renderer = new THREE.WebGLRenderer();
+        let attempt = document.getElementById("attempt")
+        if (attempt) {
+            renderer.setSize(attempt.clientWidth, attempt.clientHeight);
+            attempt.appendChild(renderer.domElement)
+        }
+
+    }
+
     render() {
         return (
-            <h1>Try It</h1>
+            <>
+                <div id="attempt"></div>
+            </>
         )
     }
 }
